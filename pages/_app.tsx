e8cc24e9a16bf9dc24ca95cd9d193de52929503d
theGobindSingh <android.gobind@gmail.com>
2022-12-11 02:32:58 +0530

    product page done /// about page & mobile view for all pages yet to be done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ import {
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60 * 1,
+      staleTime: 1000 * 60 * 10, //10 mins,
       // staleTime: 1000 * 5,
     },
   },
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function index() {
-  return <div>inside about</div>;
+  return (
+    <section id="About-page">
+      <h1>About vintageinkk and adeeba /// Under development</h1>
+    </section>
+  );
 }
